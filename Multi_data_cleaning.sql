9c091be8c783e710a83ef8dc91bcb9bbe9dc7067
juclart Ngouedi <ladeche17@outlook.com>
2024-03-19 00:16:49 +0000

    V1

--- Multi_data_cleaning.sql
+++ Multi_data_cleaning.sql
@@ -8,14 +8,14 @@ CREATE EXTENSION "uuid-ossp";
 
 ALTER TABLE orders_table
 
-	ALTER COLUMN date_uuid TYPE UUID USING (uuid_generate_v4()),
-	ALTER COLUMN user_uuid TYPE UUID USING (uuid_generate_v4()),
+	ALTER COLUMN date_uuid TYPE UUID USING date_uuid::uuid,
+	ALTER COLUMN user_uuid TYPE UUID USING user_uuid::uuid,
 	ALTER COLUMN store_code TYPE VARCHAR(25),
 	ALTER COLUMN product_code TYPE VARCHAR(25),
 	ALTER COLUMN product_quantity TYPE SMALLINT,
 	ALTER COLUMN card_number TYPE VARCHAR(19);
 
-
+ALTER COLUMN uuid_column TYPE UUID USING column_name::uuid
 /* UPDATING DIM_USERS TABLE*/
 
 SELECT * FROM dim_users
@@ -25,7 +25,7 @@ ALTER TABLE dim_users
 	ALTER COLUMN last_name TYPE VARCHAR(255),
 	ALTER COLUMN date_of_birth TYPE DATE,
 	ALTER COLUMN country_code TYPE VARCHAR(2),
-	ALTER COLUMN user_uuid TYPE UUID USING (uuid_generate_v4()),
+	ALTER COLUMN user_uuid TYPE UUID USING user_uuid::uuid,
 	ALTER COLUMN join_date TYPE DATE;
 
 
